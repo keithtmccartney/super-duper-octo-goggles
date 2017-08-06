d76daf840f0b0c8a20cb5bced24ba48be18869ed
Keith <keithtmccartney@hotmail.com>
2017-08-06 20:59:52 +0100

    Applying argument logic adjustments.

---
+++ super-duper-octo-goggles/super-duper-octo-goggles/Helpers/HelperCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace super_duper_octo_goggles.Helpers
+{
+    public static class HelperCollection
+    {
+        public static bool IsNumeric(object value)
+        {
+            try
+            {
+                int _value;
+
+                bool _IsNumber = int.TryParse(Convert.ToString(value), System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out _value);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+    }
+}
--- super-duper-octo-goggles/super-duper-octo-goggles/Program.cs
+++ super-duper-octo-goggles/super-duper-octo-goggles/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using super_duper_octo_goggles.Helpers;
 
 namespace super_duper_octo_goggles
 {
@@ -10,16 +11,27 @@ namespace super_duper_octo_goggles
     {
         static void Main(string[] args)
         {
-            if (args.Length == 2) //This was originally going to be: args.Length != 0 && args.Length == 2.
+            if (args.Length == 2) //This was originally going to be: args.Length != 0 && args.Length == 2;
             {
                 var market_file = "";
                 int loan_amount = 0;
 
                 for (int i = 0; i < args.Length; i++)
                 {
-                    if (string.Compare(args[1], "market.csv", true) == 0) //This was originally going to be: args[1] == "market.csv".
+                    if (string.Compare(args[0], "market.csv", true) == 0) //This was originally going to be: args[1] == "market.csv"; this could be added to the Helper Collection;
                     {
-                        Console.WriteLine("Please enter a numeric argument.");
+                        if (HelperCollection.IsNumeric(args[1]) == true)
+                        {
+                            i++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a correct loan amount, e.g. cmd> quote.exe market.csv 1500.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a correct market file, e.g. cmd> quote.exe market.csv 1500.");
                     }
                 }
             }
